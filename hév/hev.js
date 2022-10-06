f2f1634beffbd89eff7177201514dad2f1e02f9a
Linder Martin <linderm@KKSZKI.LOCAL>
2022-10-06 13:12:00 +0200

    java kesz

---
+++ hév/hev.js
@@ -0,0 +1,13 @@
+function szamol()
+{
+    var indulo=document.getElementById("select1").value;
+    var erkezo=document.getElementById("select2").value;
+
+    var eredmeny=indulo-erkezo;
+    if(indulo==érkezo){
+        alert("Hiba:azonos megallokat valaszd ki");
+    }
+    else{
+}
+    alert(Math.abs(eredmeny));
+}
