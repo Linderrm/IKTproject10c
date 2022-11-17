52800d57b7ebbf0ec7b0444246b1269d33e97747
Linder Martin <linderm@KKSZKI.LOCAL>
2022-11-17 11:17:53 +0100

    1.feladat

---
+++ L_M elágazások/elágazások.py
@@ -0,0 +1,12 @@
+#1
+
+State=input("jó napod van")
+if State=="igen":
+    ihprint("igen")
+elif State=="igen" :
+    print("nem")
+else:
+    print("ilyen válsz nincs")        
+        
+
+#3
---
+++ python/python.py
@@ -0,0 +1,40 @@
+list = []
+
+szam1 = int(input("Adj meg egy számot"))
+szam2 = int(input("Adj meg még egy számot"))
+szam3 = int(input("Adj meg egy harmadik számot"))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(min(list))
+
+
+kisebb()
+
+
+list = []
+
+szam1 = int(input("Adj meg egy számot"))
+szam2 = int(input("Adj meg még egy számot"))
+szam3 = int(input("Adj meg egy harmadik számot"))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def nagyobb():
+    print(max(list))
+
+
+nagyobb() 
+
+
+
+
+
+
+
+
