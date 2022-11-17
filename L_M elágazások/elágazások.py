74c2d3cf9311c116ad0c1b02e8d2ae067927fb4b
Linder Martin <linderm@KKSZKI.LOCAL>
2022-11-17 12:00:34 +0100

    3.feladat

--- L_M elágazások/elágazások.py
+++ L_M elágazások/elágazások.py
@@ -1,4 +1,7 @@
-#1
+
+import random
+
+#1------------------------------------
 
 """
 State=input("jó napod van")
@@ -10,10 +13,27 @@ else:
     print("ilyen válsz nincs")        
 """      
 
-#2
-
+#2-------------------------------
+"""
 Number=int(input("kérek egy számpot"))
 if(Number%2==0):
     print("szám páros")
 else:
     print("páratlan")    
+    """
+
+    #3--------------------------------------
+"""
+
+szám =random.randint(1,6)
+szám1=int(input("kérek egy számot"))
+print(szám)
+
+if(szám1<szám):
+    print("kisebb")
+elif(szám1>szám):
+    print("kisebb")
+else:
+    print("egyenlő")        
+"""
+
